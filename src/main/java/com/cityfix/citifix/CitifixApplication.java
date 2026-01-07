13844b6e6b4fd53d2039f8cc95bf7fa1e257b97e
eariasvalor <eariasvalor@gmail.com>
2026-01-07 15:56:00 +0100

    build: setup project with Spring Boot 3, Docker MySQL and Testcontainers configuration

---
+++ src/main/java/com/cityfix/citifix/CitifixApplication.java
@@ -0,0 +1,13 @@
+package com.cityfix.citifix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitifixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitifixApplication.class, args);
+	}
+
+}
